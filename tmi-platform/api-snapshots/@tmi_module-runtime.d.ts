f59cb97d164554740dcdac9add1de1dda810d50f
Marcel Dickens <berntmusic33@gmail.com>
2026-05-15 11:05:39 -0700

    chore: update api snapshots after Phase 1 wiring

---
+++ tmi-platform/api-snapshots/@tmi_module-runtime.d.ts
@@ -0,0 +1,10 @@
+export * from "./types.js";
+export * from "./RuntimeController.js";
+export * from "./StimulationEngine.js";
+export * from "./HealthWatcher.js";
+export * from "./RecoveryController.js";
+export * from "./EventBus.js";
+export * from "./BootController.js";
+export * from "./Bots.js";
+export * from "./EnvironmentConfig.js";
+//# sourceMappingURL=index.d.ts.map
